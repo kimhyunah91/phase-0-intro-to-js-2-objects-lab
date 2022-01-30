495c7dd8d676775fc7e31d051ff93b2fef089df4
Christine Kim <kimhyunah1991@gmail.com>
2022-01-29 21:26:33 -0500

    commit tree b40c162efca12c8f53d80400e68141b8b1407fb9

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+const employee = {
+    "name": "Jim",
+    "streetAddress": "",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, "name": `Sam`}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [streetAddress]: `11 Broadway`}
+}
+
